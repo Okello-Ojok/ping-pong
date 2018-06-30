500976fac74eb2baa945fe73d482be9236f5dfca
Okello-Ojok Emmanuel <emmanuel.alvino.okello06@gmail.com>
2018-06-30 10:05:53 -0700

    add comments in the script file

--- js/scripts.js
+++ js/scripts.js
@@ -13,35 +13,39 @@ let pingPong = function(num) {
 //Make a loop to iterate through from 1 to the number entered by the user.
 
   for (let counter = 1; counter <= num; counter++) {
-    //If
+    //Condition to test if number is divisible by 15 and print pingpong which is stored in variable str3
     if (counter % 15 === 0) {
       results.push(str3);
     }
+    //Condition to test if number is divisible by 5 and print pong which is stored in variable str2
     else if (counter % 5 === 0) {
       results.push(str2);
     }
+    //Condition to test if number is divisible by 3 and print ping which is stored in variable str1
     else if (counter % 3 === 0) {
       results.push(str1);
-    } else {
+    }
+    //If all above conditions are false, print the number itself
+    else {
       results.push(counter);
     }
   }
-  return results;
+  return results;    //Return the array now populated with values from all the above conditions
 
 }
 
 //UI Logic
 
 $(document).ready(function() {
-  $("form#ping-pong").submit(function(event) {
-    event.preventDefault();
+  $("form#ping-pong").submit(function(event) {        //Add submit method to the form
+    event.preventDefault();                           //Prevent default behaviour of form
 
-
+    //Get user input from the form and assign it to a variable
     let userNum = parseInt($("#userInput").val());
-
+    //Call the function and pass in the user input above as an argument and assign this to a variable
     let display = pingPong(userNum);
-
-    display.forEach(function(number) {
+    //Since the variable holding the function output is an array, loop through each item with forEach
+    display.forEach(function(number) {                      //and print each number as a list by appending it.
       $("#outPut").append("<li>" + number + "</li>");
     });
   });
